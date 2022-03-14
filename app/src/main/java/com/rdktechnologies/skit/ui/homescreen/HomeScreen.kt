7653c453396b58714844aae55cdef176f8390cd0
Rupesh Kumar deshmukh <rupeshdeshmukh618@gmail.com>
2022-03-15 01:08:48 +0530

    splashscreen completed

---
+++ app/src/main/java/com/rdktechnologies/skit/ui/homescreen/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.rdktechnologies.skit.ui.homescreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rdktechnologies.skit.R
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
--- app/src/main/java/com/rdktechnologies/skit/ui/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/rdktechnologies/skit/ui/splashscreen/SplashScreen.kt
@@ -1,10 +1,14 @@
 package com.rdktechnologies.skit.ui.splashscreen
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.rdktechnologies.skit.R
+import com.rdktechnologies.skit.ui.homescreen.HomeScreen
 
 
 @SuppressLint("CustomSplashScreen")
@@ -12,7 +16,13 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        setTransparentStatusBar()
+            //setTransparentStatusBar()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeScreen::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 
    private fun setTransparentStatusBar() {
