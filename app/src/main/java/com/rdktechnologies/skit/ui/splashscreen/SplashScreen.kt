e190b8709c2845d82103d632a5d5361b8da2ffd2
Rupesh Kumar deshmukh <rupeshdeshmukh618@gmail.com>
2022-03-14 22:08:06 +0530

    minor changes

--- app/src/main/java/com/rdktechnologies/skit/ui/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/rdktechnologies/skit/ui/splashscreen/SplashScreen.kt
@@ -1,12 +1,24 @@
 package com.rdktechnologies.skit.ui.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.rdktechnologies.skit.R
 
+
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        setTransparentStatusBar()
+    }
+
+   private fun setTransparentStatusBar() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
