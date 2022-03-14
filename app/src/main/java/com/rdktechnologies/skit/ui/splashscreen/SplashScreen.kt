c1186f21f45a75d6fd102b594f96ed876d826b3c
Rupesh Kumar deshmukh <rupeshdeshmukh618@gmail.com>
2022-03-14 12:15:08 +0530

    initial commit

---
+++ app/src/main/java/com/rdktechnologies/skit/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.rdktechnologies.skit.ui.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rdktechnologies.skit.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
